dc452638530e22ebf2832606e007878c19e3e3a7
Marco Mangan <marco.mangan@pucrs.br>
2016-04-25 22:03:14 -0300

    Segundo commit! Teste do Hello!

--- src/br/pucrs/alpro3/Hello.java
+++ src/br/pucrs/alpro3/Hello.java
@@ -2,6 +2,6 @@ package br.pucrs.alpro3;
 
 public class Hello {
 	public static void main(String[] args) {
-		System.out.println("Estou no GitHub!");
+		System.out.println("Estou no GitHub, de novo!");
 	}
 }
