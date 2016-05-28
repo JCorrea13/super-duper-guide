4b99c04c1da74ba44afc2df905c4bfffbf6c15db
Marco Mangan <marco.mangan@pucrs.br>
2016-05-27 21:40:19 -0300

    Matriz, arestas e toString.

--- src/br/pucrs/alpro3/grafos/Grafo.java
+++ src/br/pucrs/alpro3/grafos/Grafo.java
@@ -1,5 +1,33 @@
 package br.pucrs.alpro3.grafos;
 
+/**
+ * Grafo direcionado com arestas não valoradas.
+ * 
+ * @author marco.mangan@pucrs.br
+ *
+ */
 public class Grafo {
 
+	private int dados[][];
+		
+	public Grafo(int tamanho) {
+		dados = new int[tamanho][tamanho];
+	}
+		
+	public void addAresta(int origem, int destino) {
+		dados[origem][destino] = 1;
+	}
+	
+	@Override
+	public String toString() {
+		String s = "";
+		for (int i = 0; i < dados.length; i++) {
+			for (int j = 0; j < dados.length; j++) {
+				s += String.format("%d ",dados[i][j]);
+			}
+			s += '\n';
+		}
+		return s;
+	}
+	
 }
--- src/br/pucrs/alpro3/grafos/GrafoApp.java
+++ src/br/pucrs/alpro3/grafos/GrafoApp.java
@@ -4,5 +4,14 @@ public class GrafoApp {
 
 	public static void main(String[] args) {
 		System.out.println("Grafos");
+		
+		Grafo g = new Grafo(5);
+		
+		g.addAresta(0, 4);
+		g.addAresta(4, 0);
+		
+		System.out.println(g);
+		
+		
 	}
 }
